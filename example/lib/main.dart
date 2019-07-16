2af1ff5afad082e15ce15fdbabd03d14f72b3e54
Gonçalo Palma <solid.goncalo@gmail.com>
2019-07-16 16:55:51 +0100

    Added more customization to the nip location Added nip size

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,36 +16,50 @@ class _MyAppState extends State<MyApp> {
         appBar: new AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: new Center(
-          child: new SpeechBubble(
-            nipLocation: NipLocation.BOTTOM,
-            // child: Column(
-            //   mainAxisSize: MainAxisSize.min,
-            //   children: <Widget>[
-            //     Text("Give your users some guided instruction"),
-            //     Text("From the inside of a Speech Bubble")
-            //   ],
-            // ),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Icon(
-                  Icons.favorite,
-                  color: Colors.white,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: new SpeechBubble(
+                nipLocation: NipLocation.BOTTOM,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Icon(
+                      Icons.favorite,
+                      color: Colors.white,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                    ),
+                    Text(
+                      "1",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(4.0),
-                ),
-                Text(
-                  "1",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18.0,
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
+            SizedBox(height: 20,),
+            Center(
+              child: new SpeechBubble(
+                nipLocation: NipLocation.BOTTOM_LEFT,
+                nipHeight: 20,
+                child: Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                  Text(
+                    "You can also change the nip size!",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                    ),
+                  )
+                ]),
+              ),
+            )
+          ],
         ),
       ),
     );
